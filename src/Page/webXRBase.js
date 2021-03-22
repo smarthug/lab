ea7b0bc886bf1cc5d4d2e60c2f4b545f8793f8c4
smarthug <kirklayer@gmail.com>
2021-03-22 17:30:16 +0900

    webxr setup

---
+++ src/Page/webXRBase.js
@@ -0,0 +1,64 @@
+import * as THREE from "three";
+import React, { useEffect, useRef } from "react";
+
+import CameraControls from "camera-controls";
+import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
+
+CameraControls.install({ THREE: THREE });
+
+let cube, scene, camera, renderer, cameraControls;
+const clock = new THREE.Clock();
+
+export default function Main() {
+  const containerRef = useRef();
+  useEffect(() => {
+    Init();
+    Animate();
+    
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function Init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.xr.enabled = true;
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    containerRef.current.appendChild(renderer.domElement);
+
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshNormalMaterial();
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    camera.position.z = 5;
+
+    cameraControls = new CameraControls(camera, renderer.domElement);
+
+    document.body.appendChild(VRButton.createButton(renderer));
+  }
+
+  function Animate() {
+    // requestAnimationFrame(Animate);
+    renderer.setAnimationLoop(Render);
+    
+  }
+
+  function Render() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    const delta = clock.getDelta();
+    // const hasControlsUpdated = cameraControls.update(delta);
+    cameraControls.update(delta);
+    console.log("check")
+
+    renderer.render(scene, camera);
+  }
+
+  return <div ref={containerRef}></div>;
+}
