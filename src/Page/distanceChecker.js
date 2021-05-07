2ebf0da76cbefe4d8df1dea5a355cde279324cdf
hosuk <kirklayer@gmail.com>
2021-05-07 16:57:39 +0900

    testing works , wasm loaders

--- src/Page/distanceChecker.js
+++ src/Page/distanceChecker.js
@@ -4,12 +4,18 @@ import React, { useEffect, useRef } from "react";
 import CameraControls from "camera-controls";
 
 import InfiniteGridHelper from './InfiniteGridHelper'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { KTX2Loader } from 'three/examples/jsm/loaders/KTX2Loader.js';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
+import { MeshoptDecoder } from 'three/examples/jsm/libs/meshopt_decoder.module.js';
 
 
 CameraControls.install({ THREE: THREE });
 
 let cube, scene, camera, renderer, cameraControls;
 const clock = new THREE.Clock();
+
+let tree;
 
 export default function Main() {
     const containerRef = useRef();
@@ -50,7 +56,7 @@ export default function Main() {
         var geometry = new THREE.BoxGeometry(1, 1, 1);
         var material = new THREE.MeshNormalMaterial();
         cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
+        // scene.add(cube);
         camera.position.z = 5;
 
         cameraControls = new CameraControls(camera, renderer.domElement);
@@ -58,6 +64,11 @@ export default function Main() {
         const grid = new InfiniteGridHelper(10, 100);
 
         scene.add(grid)
+
+
+        LightSetUp();
+        Loader();
+
     }
 
     function Animate() {
@@ -73,7 +84,123 @@ export default function Main() {
     }
 
     return <div ref={containerRef}>
+        <button style={{ position: "absolute" }} onClick={Mover}>Mover</button>
         <canvas ref={canvasRef} />
         <div ref={vrButtonConRef}></div>
     </div>;
+}
+
+//'model/bronze_replica_statue_of_liberty/scene.gltf',
+//
+
+function Loader() {
+    // loader.load(
+    //     'model/tree.gltf',
+    //     (gltf) => {
+
+
+
+    //         // gltf.scene.traverse(n => {
+    //         //     if (n.isMesh) {
+    //         //         n.castShadow = true;
+    //         //         n.receiveShadow = true;
+    //         //         if (n.material.map) n.material.map.anisotropy = 16;
+    //         //     }
+    //         // });
+    //         console.log(gltf)
+    //         scene.add(gltf.scene);
+
+
+
+    //         // scene.add(camera)
+
+    //         //gltf.animations; // Array<THREE.AnimationClip>
+    //         //gltf.scene; // THREE.Group
+    //         //gltf.scenes; // Array<THREE.Group>
+    //         //gltf.cameras; // Array<THREE.Camera>
+    //         //gltf.asset; // Object
+
+    //     },
+    //     // called while loading is progressing
+    //     function (xhr) {
+
+    //         console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+    //     },
+    //     // called when loading has errors
+    //     function (error) {
+
+    //         console.log('An error happened');
+
+    //     }
+    // );
+
+
+
+
+    const loader = new GLTFLoader()
+        // .setCrossOrigin('anonymous')
+        .setDRACOLoader(
+            new DRACOLoader().setDecoderPath('assets/wasm/')
+        )
+        .setKTX2Loader(
+            new KTX2Loader()
+                .setTranscoderPath('assets/wasm/')
+                .detectSupport(renderer)
+        )
+        .setMeshoptDecoder(MeshoptDecoder);
+
+    const blobURLs = [];
+
+    loader.load('model/tree_textures.gltf', (gltf) => {
+
+        const scene2 = gltf.scene || gltf.scenes[0];
+        const clips = gltf.animations || [];
+
+        if (!scene2) {
+            // Valid, but not supported by this viewer.
+            throw new Error(
+                'This model contains no scene, and cannot be viewed here. However,'
+                + ' it may contain individual 3D resources.'
+            );
+        }
+
+        // this.setContent(scene, clips);
+
+        // blobURLs.forEach(URL.revokeObjectURL);
+
+        // See: https://github.com/google/draco/issues/349
+        // DRACOLoader.releaseDecoderModule();
+
+        // resolve(gltf);
+        scene.add(scene2)
+        tree = scene2;
+        tree.scale.set(10,10,10)
+
+    });
+}
+
+function LightSetUp() {
+    const light = new THREE.AmbientLight(0x404040); // soft white light
+    scene.add(light);
+
+    // White directional light at half intensity shining from the top.
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    scene.add(directionalLight);
+
+
+    const hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 4);
+    scene.add(hemiLight);
+
+
+    const spotLight = new THREE.SpotLight(0xffa95c, 4);
+    spotLight.position.set(-50, 350, 50);
+    spotLight.castShadow = true;
+    scene.add(spotLight);
+}
+
+
+function Mover(){
+    // tree.position.set(0,0,10)
+    tree.position.add(new THREE.Vector3(0,0,-10))
 }
